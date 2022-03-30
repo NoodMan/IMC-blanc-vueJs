4b03608d602cc16ff4cc3422e15e2519f3f5d10b
NoodMan <camara.adeline@yahoo.fr>
2022-03-31 01:18:31 +0200

    add css

---
+++ IMC-blanc-vueJs/src/components/Table_Imc.vue
@@ -0,0 +1,76 @@
+  
+<template>
+  <center>
+    <br />
+  
+    <div class="card" style="">
+      <br />
+
+      <table class="table">
+       
+          <tr>
+            <th scope="col">
+              Taille(cm) <span class="material-icons"> straighten </span>
+            </th>
+            <th scope="col">
+              Poids(kg) <span class="material-icons"> scale </span>
+            </th>
+            <th scope="col">
+              IMC <span class="material-icons"> equalizer </span>
+            </th>
+          </tr>
+     
+        <tbody>
+          <tr>
+            <td class="table-active">{{ store.taille }}</td>
+            <td class="table-active">{{ store.poids }} kg</td>
+            <td>{{ store.imc }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+    </div>
+  </center>
+  <br />
+  <center>
+   <span class="material-icons"> flaky </span> 
+   </center>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script setup lang="ts">
+//pour pouvoir utiliser les données du store il faut déjà un import
+import { useImcStore } from "@/stores/imc";
+//et ne pas oublierle const
+const store = useImcStore();
+</script>
+
+
+<style>
+table {
+  margin: 10px;
+}
+table,
+th,
+td {
+  border: 1px solid;
+}
+td,
+tr {
+  width: 600px;
+  padding: 20px;
+}
+</style>
--- IMC-blanc-vueJs/src/main.ts
+++ IMC-blanc-vueJs/src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
 
-import App from "./App.vue";
-import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css"; 
 
-const app = createApp(App);
+import { createPinia } from 'pinia'
 
-app.use(createPinia());
-app.use(router);
+import App from './App.vue'
+import router from './router'
 
-app.mount("#app");
+const app = createApp(App)
+
+app.use(createPinia())
+
+
+app.use(router)
+
+app.mount('#app')
